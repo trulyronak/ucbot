c46f45786091c9404ac4d56decd6e6c635e98006
Matei-Alexandru Gardus <matei@gard.us>
2022-02-14 21:38:32 -0800

    Fix stupid Discord.js bugs I forgot

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
+  if (msg.author.bot) return;
   const cleanedWords = msg.content.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").split(/\W+/);
   const culprits = ucbWords.filter(val => cleanedWords.includes(val));
 
-  if (cleanedWords.includes("UCB") || cleanedWords.includes(" UCB")) {
+  if (cleanedWords.includes("ucb")) {
     if (culprits.length == 0) {
       msg.react('❤️');
     } else {
