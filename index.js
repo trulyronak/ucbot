0ac77861cca061d22f76eab377d6f5a7cf3f1c56
Kunal Singla <tekunalogy@gmail.com>
2022-02-14 22:05:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ global.Discord = Discord;
 const bot = new Discord.Client();
 global.bot = bot;
 
-const ucbWords = ["berk", "berkeley", "berekeley", "cal", "ucberkeley", "berkley", "ucberekely", "ucberkeley"]
+const ucbWords = ["berk", "berkeley", "berekeley", "cal", "berkley", "ucberkeley", "ucberekely", "ucberkley"]
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
