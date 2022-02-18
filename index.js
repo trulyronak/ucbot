ca7710b459434dc8c821b17aff21b5d4af4e839c
Ronak Shah <ronakshahultimate@gmail.com>
2022-02-17 23:00:35 -0800

    fixed bug where permissions broke shit

--- index.js
+++ index.js
@@ -13,24 +13,27 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
+bot.on('message', async msg => {
   try {
-    if (msg.author.bot) return;
+    const canReplyInChannel = msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES', false);
+    if (msg.author.bot || !canReplyInChannel) return;
     const cleanedWords = msg.content.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").split(/\W+/);
     const culprits = ucbWords.filter(val => cleanedWords.includes(val));
 
     if (cleanedWords.includes("ucb")) {
       if (culprits.length == 0) {
-        msg.react('❤️');
+        await msg.react('🇺');
+        await msg.react('🇨');
+        await msg.react('🇧');
       } else {
-        msg.reply(`Thanks for referring to UCB as UCB on the internet, but make sure to be consistent; always do it.
+        msg.reply(`[debug] Thanks for referring to UCB as UCB on the internet, but make sure to be consistent; always do it.
   I am a bot, and this action was performed automatically. Please contact Daddy Khosla if you have any questions or concerns.`);
       }
       return;
     }
 
     if (culprits.length > 0) {
-      msg.reply(`Please refer to UCB as UCB on the internet.
+      msg.reply(`[debug] Please refer to UCB as UCB on the internet.
   I am a bot, and this action was performed automatically. Please contact Daddy Khosla if you have any questions or concerns.`)
     }
   } catch (err) {
